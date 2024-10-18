536a9f1e7709198e61367ff82b76b8e9b4c8388b
karukavina <karukavina@gmail.com>
2024-10-18 19:07:34 -0400

    fixed admin page

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,  get_object_or_404
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from .models import Profile
@@ -13,7 +13,8 @@ def home(request):
 @login_required
 def main(request):
     profile, created = Profile.objects.get_or_create(user=request.user)
-    return render(request, "main.html", {'user': request.user}) 
+    return render(request, "main.html", {'user':request.user, 'profile' : profile})
+   
 
 def logout_view(request):
     logout(request)
