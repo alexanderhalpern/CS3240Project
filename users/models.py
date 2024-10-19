9bc7f53138a6215f1fb83b795087ceb56925cd4f
karukavina <karukavina@gmail.com>
2024-10-18 20:53:30 -0400

    personalized profile

--- users/models.py
+++ users/models.py
@@ -4,8 +4,9 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True)
+    bio = models.TextField(blank=True, null=True)
     is_pma_admin = models.BooleanField(default=False)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
 
     def __str__(self):
         return f"{self.user.username}'s profile"
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect,  get_object_or_404
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from .forms import ProfileUpdateForm
 
 
 def home(request):
@@ -14,6 +15,24 @@ def home(request):
 def main(request):
     profile, created = Profile.objects.get_or_create(user=request.user)
     return render(request, "main.html", {'user':request.user, 'profile' : profile})
+
+@login_required
+def update_profile(request):
+    profile = Profile.objects.get(user=request.user)
+   
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            first_name = request.POST.get('first_name')
+            if first_name: 
+                request.user.first_name = first_name
+                request.user.save() 
+            profile.save() 
+            return redirect('main')  
+    else:
+        form = ProfileUpdateForm(instance=profile)  
+    return render(request, 'update_profile.html', {'form': form, 'first_name': request.user.first_name}) 
    
 
 def logout_view(request):
