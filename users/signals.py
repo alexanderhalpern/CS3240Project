395f18a33d74f5186989aa5bec943899d3a8088f
joncantcode <jonfox.work@gmail.com>
2024-12-01 21:12:40 -0500

    added signal for when admin is added, added signal for when event is created

--- users/signals.py
+++ users/signals.py
@@ -19,6 +19,7 @@ def assign_role_on_login(sender, request, user, **args):
     profile.save()
 
 @receiver(m2m_changed, sender=CIO.members.through)
+#signal for when a member is added to the club
 def notify_member_added(sender, instance, action, reverse, pk_set, **kwargs):
     if action == "post_add":
         for user_id in pk_set:
@@ -26,4 +27,26 @@ def notify_member_added(sender, instance, action, reverse, pk_set, **kwargs):
             Notification.objects.create(
                 user=user,
                 content=f"You have been added as a member to the club: {instance.name}"
+            )
+
+@receiver(m2m_changed, sender=CIO.admins.through)
+#signal for when a new club admin is added
+def notify_admin_added(sender, instance, action, reverse, pk_set, **kwargs):
+    if action == "post_add":
+        for user_id in pk_set:
+            user = User.objects.get(pk=user_id)
+            Notification.objects.create(
+                user=user,
+                content=f"You have been made an admin of the club: {instance.name}"
+            )
+
+@receiver(post_save, sender=Event)
+#signal for when a new event is created in a club that the user is a member of
+def notify_event_created(sender, instance, created, **kwargs):
+    if created:
+        members = instance.cio.members.all()
+        for member in members:
+            Notification.objects.create(
+                user=member,
+                content=f"A new event has been scheduled in the club {instance.cio.name}: {instance.name} on {instance.date} at {instance.time}."
             )
