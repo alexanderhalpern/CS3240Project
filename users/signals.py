6d047eacce646788d5bea7f2be4b0f7714384d24
joncantcode <jonfox.work@gmail.com>
2024-12-01 21:06:18 -0500

    added signal to notify member when added to a club

--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,9 @@
 from allauth.account.signals import user_logged_in
 from django.dispatch import receiver
 from .models import Profile
+from django.db.models.signals import m2m_changed, post_save
+from django.contrib.auth.models import User
+from .models import CIO, Event, Notification
 
 @receiver(user_logged_in)
 def assign_role_on_login(sender, request, user, **args):
@@ -14,3 +17,13 @@ def assign_role_on_login(sender, request, user, **args):
         profile.is_pma_admin = False
 
     profile.save()
+
+@receiver(m2m_changed, sender=CIO.members.through)
+def notify_member_added(sender, instance, action, reverse, pk_set, **kwargs):
+    if action == "post_add":
+        for user_id in pk_set:
+            user = User.objects.get(pk=user_id)
+            Notification.objects.create(
+                user=user,
+                content=f"You have been added as a member to the club: {instance.name}"
+            )
