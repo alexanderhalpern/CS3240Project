44c90bccb0e7872caadad530cc1b709b3e3172c0
karukavina <karukavina@gmail.com>
2024-12-04 19:58:24 -0500

    admin site update

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'bio', 'is_pma_admin', 'profile_picture'] 
+    list_filter = ['is_pma_admin'] 
+    search_fields = ['user__username', 'user__email']   
+admin.site.register(Profile, ProfileAdmin)
